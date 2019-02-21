0cf3194dd9bba58a0e2b2b7a1bb5dbb54830d93d
Bernardo (UC3M) <bernardo.dauria@uc3m.es>
2019-02-21 15:28:25 +0100

    make paths relative in clean_data_280.R

--- clean_data_280.R
+++ clean_data_280.R
@@ -5,7 +5,7 @@ library(xml2)
 library(tidyverse)
 library(readr)
 
-pg <- read_xml("~/Documents/TUe/Jaar 6/Internship/Assignment/Data/280.xml")
+pg <- read_xml("Data/280.xml")
 
 f <- function(u)
   map_df(xml_children(u), ~list(variable=xml_name(.x), value=xml_text(.x))) %>% spread("variable","value")
