5ffeb0f55a5fb142fb495f0c9f46336759bebf00
Jordi Tijsse Claase <jorditijsseclaase@MacBook-Pro-van-Jordi.local>
2019-03-11 12:47:41 +0100

    Separated files of tidying and analysis of weather

---
+++ Weather_analysis.R
@@ -0,0 +1,26 @@
+#Read weather data
+
+rm(list=ls())
+
+library(tidyverse)
+library(chron)
+library(writexl)
+
+#load data
+library("readxl")
+
+weather <- read_excel("weather2013-2018.xlsx")
+
+
+#PLOTS
+#plot of average Temperature (2013-2018)
+ggplot(data = weather) +
+  geom_point(mapping = aes(x = Date, y = Avg_T, color=Season)) +
+  ggtitle("Average temperature (2013-2018)")
+
+
+#plot of average Wind velocity (2013-2018)
+ggplot(data = weather) +
+  geom_point(mapping = aes(x = Date, y = Avg_vel)) +
+  ggtitle("Average Wind velocity (2013-2018)")
+
