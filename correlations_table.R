2ea7738dda8321057c786ff47107a6631fc87d81
jorditijsseclaase <jordi.tijsseclaase@gmail.com>
2019-03-27 12:31:52 +0100

    update

--- correlations_table.R
+++ correlations_table.R
@@ -40,11 +40,48 @@ ggplot(data = Total) +
 
 
 # Make a model
+library(mgcv)
+
+# NO2 with NO per day 65,3 %
+mod1 = gam(NO2~NO+s(Total$DayNr,k=20,bs="ps"), data=Total)
+summary(mod1)
+plot(mod1)
+
+# NO2 with T_max per day 36,2%
+mod2 = gam(NO2~T_max+s(Total$DayNr,k=20,bs="ps"), data=Total)
+summary(mod2)
+plot(mod2)
+
+# NO2 with wind speed
+mod3 = gam(NO2~Avg_vel+s(Total$DayNr, k=40, bs="ps"), data=Total)
+summary(mod3)
+plot(mod3)
+
+# PM10 with T_max 30,8%,, this works for all the polutants between 30 and 40%
+mod4 = gam(PM10~T_max+s(Total$DayNr,k=20,bs="ps"), data=Total)
+summary(mod4)
+plot(mod4)
+
+# PM10 with T_max 66,9% but not significant? This is the same for alle the polutants in combination
+# with all the traffic data..
+mod5 = gam(O3~users_Street30+s(Total$DayNr,k=20,bs="ps"), data=Total)
+summary(mod5)
+plot(mod5)
 
 
-mod = gam(NO2~NO+s(Total$DayNr,k=20,bs="ps"), data=Total)
-summary(mod)
-plot(mod)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
