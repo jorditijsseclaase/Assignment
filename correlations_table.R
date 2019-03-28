77f316a02ce08cce72e68b208ef3f87e5d382769
jorditijsseclaase <jordi.tijsseclaase@gmail.com>
2019-03-28 12:39:50 +0100

    update

--- correlations_table.R
+++ correlations_table.R
@@ -27,10 +27,34 @@ Total$Season <- as.numeric(Total$Season)
 Total$day <- NULL
 Total$Date <- NULL
 
-library(dplyr)
 
+
+library(dplyr)
 #Correlations table
 cor <- cor(Total, use = "pairwise.complete.obs")
+
+
+# Add T with delay
+Total$T_max_d <- lag(Total$T_max, n=1L)
+Total$T_min_d <- lag(Total$T_min, n=1L)
+Total$Avg_T_d <- lag(Total$Avg_T, n=1L)
+
+# Add polutans with delay
+Total$SO2_d <- lag(Total$SO2, n=1L)
+Total$CO_d <- lag(Total$CO, n=1L)
+Total$NO2_d <- lag(Total$NO2, n=1L)
+Total$NO_d <- lag(Total$NO, n=1L)
+Total$PM2.5_d<- lag(Total$PM2.5, n=1L)
+Total$PM10_d <- lag(Total$PM10, n=1L)
+Total$NOx_d <- lag(Total$NOx, n=1L)
+Total$O3_d <- lag(Total$O3, n=1L)
+Total$TOL_d <- lag(Total$TOL, n=1L)
+Total$BEN_d <- lag(Total$BEN, n=1L)
+Total$EBE_d <- lag(Total$EBE, n=1L)
+Total$AQI_d <- lag(Total$AQI, n=1L)
+
+
+
 
 
 #plot month to users m30
@@ -68,8 +92,11 @@ mod5 = gam(O3~users_Street30+s(Total$DayNr,k=20,bs="ps"), data=Total)
 summary(mod5)
 plot(mod5)
 
-
-
+# Predict NO2, 54,1 %, O3=70% 
+mod6 = gam(NO2~Total$T_max+Total$T_max_d+Total$T_min+Total$T_min_d+Total$Avg_vel+Total$Streak_max+s(Total$users_Street30,k=20,bs="ps")+
+             s(Total$DayNr,k=20,bs="ps"), data=Total)
+summary(mod6)
+plot(mod6)
 
 
 
